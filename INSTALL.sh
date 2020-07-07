8301435eaa89728e839a5126a89616627dfd4370
Nick Engmann <cyengmann@gmail.com>
2020-07-07 00:03:43 -0400

    working autostart

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,11 @@
+echo "Make sure paths are correct:"
 mkdir -p /home/pi/piDSLR/icon
 cp -rf icon /home/pi/piDSLR
 cp dropbox_upload.py /home/pi/piDSLR/dropbox_upload.py
 cp pidslr.py /home/pi/piDSLR/pidslr.py
-python3 -m pip install -r requirements.txt 
+echo  "Installing Pip Requirements"
+python3 -m pip install -r requirements.txt
+install startup.sh pidslr.py /usr/bin/
+echo  "Installing piDSLR AutoStart service"
+mkdir -p /home/pi/.config/autostart
+install -m 644 *.desktop /home/pi/.config/autostart/
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 piDSLR
 ===============
 
-Camera project for Raspberry Pi + camera +  MHS3.5TFT
+Camera project for Raspberry Pi 2/3 + HQ Camera +  MHS35-TFT
+
+# Installation
+
+` sudo ./INSTALL.sh`
+
+
 
