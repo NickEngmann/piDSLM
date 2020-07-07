d453f96c8edf385186a39e2876b52d6a9233644e
Nick Engmann <cyengmann@gmail.com>
2020-07-07 11:56:53 -0400

    fixed INSTALL script

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,7 +5,11 @@ cp dropbox_upload.py /home/pi/piDSLR/dropbox_upload.py
 cp pidslr.py /home/pi/piDSLR/pidslr.py
 echo  "Installing Pip Requirements"
 python3 -m pip install -r requirements.txt
-install startup.sh pidslr.py /usr/bin/
+install pidslr.py /usr/bin/
 echo  "Installing piDSLR AutoStart service"
 mkdir -p /home/pi/.config/autostart
 install -m 644 *.desktop /home/pi/.config/autostart/
+sed -i -e '$i \start_x=1\ngpu_mem=128\n' /boot/config.txt
+echo "Installation Finished, Rebooting Now"
+sleep 2
+reboot now
