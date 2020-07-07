9c85c31b739b11a1d54090f5c153b6dec0d9e013
Nick Engmann <cyengmann@gmail.com>
2020-07-06 21:57:24 -0400

    OOP

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
-adafruit-pi-cam
+piDSLR
 ===============
 
-Camera project for Raspberry Pi + camera + Adafruit PiTFT
-By PaintYourDragon (Phil B) for Adafruit Industries
-
-Read how to build this project over at 
-[http://learn.adafruit.com/diy-wifi-raspberry-pi-touch-cam](http://learn.adafruit.com/diy-wifi-raspberry-pi-touch-cam)
-Enjoy!
-
-Modified by Bernhard Bablok. For list of changes, see ChangeLog.md
-
-Not that the prereqs have changed compared to the original documentation:
-
-- python-picam    (current version, tested with 1.8)
-- python-pyexiv2  (new prereq)
+Camera project for Raspberry Pi + camera +  MHS3.5TFT
 
--- pidslr.py
+++ pidslr.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from guizero import App, PushButton, Text, Picture, Window
 from time import sleep
 import time
@@ -7,178 +8,160 @@ import sys, os
 import subprocess
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def clear():
-    show_busy()
-    os.system("rm -v /home/pi/Downloads/*")
-    hide_busy()
-    
-def show_busy():
-    busy.show()
-    print("busy now")
-    
-def hide_busy():
-    busy.hide()
-    print("no longer busy")
-    
-def fullscreen():
-    
-    app.tk.attributes("-fullscreen", True)
 
-def notfullscreen():
+class piDSLR:
+    def __init__(self):
+        self.capture_number = self.timestamp()
+        self.video_capture_number = self.timestamp()
+        self.picture_index = 0
+        self.saved_pictures = [] 
+        self.shown_picture = "" 
+      
+        GPIO.setwarnings(False) # Ignore warning for now
+        GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
+        GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(16, GPIO.FALLING, callback=self.takePicture, bouncetime=2500)
+            
+        self.app = App(layout="grid", title="Camera Controls", bg="black", width=480, height=320)
+
+        text0 = Text(self.app,color="white", grid=[1,0], text="- PiDSLR -")
+
+        button1 = PushButton(self.app, grid=[1,1], width=110, height=110, pady=35, padx=10, image="/home/pi/piDSLR/icon/prev.png", command=self.long_preview)
+        text1 = Text(self.app, color="white", grid=[1,2],text="Focus")
+
+        button2 = PushButton(self.app, grid=[3,1], width=110, height=110, pady=35, padx=10, image="/home/pi/piDSLR/icon/gallery.png", command=self.show_gallery)
+        text2 = Text(self.app, color="white", grid=[3,2],text="Gallery")
+
+        button3 = PushButton(self.app, grid=[5,1], width=110, height=110,  pady=35, padx=10, image="/home/pi/piDSLR/icon/vid.png", command=self.video_capture)
+        text2 = Text(self.app, color="white", grid=[5,2],text="HD 30s")
+
+        button4 = PushButton(self.app, grid=[7,1], width=110, height=110, pady=35, padx=10, image="/home/pi/piDSLR/icon/lapse.png", command=self.burst)
+        text3 = Text(self.app, color="white", grid=[7,2],text="Burst")
+
+        button5 = PushButton(self.app, grid=[1,3], width=110, height=110, image="/home/pi/piDSLR/icon/self.png", command=self.lapse)
+        text4 = Text(self.app, color="white", grid=[1,4],text="1h 60pix")
+
+        button6 = PushButton(self.app, grid=[3,3], width=110, height=110, image="/home/pi/piDSLR/icon/long.png", command=self.split_hd_30m)
+        text2 = Text(self.app, color="white", grid=[3,4],text="HD 30m in 5s")
+
+        button7 = PushButton(self.app, grid=[5,3], width=110, height=110, image="/home/pi/piDSLR/icon/drop.png", command=self.upload)
+        text3 = Text(self.app, color="white", grid=[5,4],text="Upload")
+
+        button8 = PushButton(self.app, grid=[7,3], width=110, height=110, image="/home/pi/piDSLR/icon/del.png", command=self.clear)
+        text4 = Text(self.app, color="white", grid=[7,4],text="Clear Folder")
+
+        self.busy = Window(self.app, bg="red",  height=175, width=480, title="busy")
+
+        self.app.tk.attributes("-fullscreen", True)
+        self.busy.hide()
+        self.app.display()
+
+    def clear(self):
+        self.show_busy()
+        os.system("rm -v /home/pi/Downloads/*")
+        self.hide_busy()
     
-    app.tk.attributes("-fullscreen", False)
+    def show_busy(self):
+        self.busy.show()
+        print("busy now")
+        
+    def hide_busy(self):
+        self.busy.hide()
+        print("no longer busy")
+        
+    def fullscreen(self):
+        self.app.tk.attributes("-fullscreen", True)
 
-# Generate timestamp string generating name for photos
-def timestamp():
-    tstring = datetime.datetime.now()
-    #print("Filename generated ...")
-    return tstring.strftime("%Y%m%d_%H%M%S")
+    def notfullscreen(self):
+        self.app.tk.attributes("-fullscreen", False)
 
-global capture_number
-capture_number = timestamp()
-video_capture_number = timestamp()
-picture_index = 0
-saved_pictures = [] 
-shown_picture = "" 
-
-def burst():
-
-    show_busy()
-    capture_number = timestamp()
-    print("Raspistill starts")
-    os.system("raspistill -rot 90 -t 10000 -tl 0 --thumb none -n -bm -o /home/pi/Downloads/BR" +str(capture_number) + "%04d.jpg")
-    print("Raspistill done")
-    hide_busy()
-    
-def split_hd_30m():
-    
-    show_busy()
-    capture_number = timestamp()
-    print("Raspivid starts")
-    os.system("raspivid -f -rot 90 -t 1800000 -sg 300000  -o /home/pi/Downloads/" +str(capture_number) + "vid%04d.h264")
-    print("done")
-    hide_busy()
+    # Generate timestamp string generating name for photos
+    def timestamp(self):
+        tstring = datetime.datetime.now()
+        #print("Filename generated ...")
+        return tstring.strftime("%Y%m%d_%H%M%S")
+  
+    def burst(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspistill starts")
+        os.system("raspistill -rot 90 -t 10000 -tl 0 --thumb none -n -bm -o /home/pi/Downloads/BR" +str(capture_number) + "%04d.jpg")
+        print("Raspistill done")
+        self.hide_busy()
+        
+    def split_hd_30m(self):   
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspivid starts")
+        os.system("raspivid -f -rot 90 -t 1800000 -sg 300000  -o /home/pi/Downloads/" +str(capture_number) + "vid%04d.h264")
+        print("done")
+        self.hide_busy()
     
 #def split_slo_1h():
 
 #def long_exp():
    
-def lapse():
-    
-    show_busy()
-    capture_number = timestamp()
-    print("Raspistill timelapse starts")
-    os.system("raspistill -rot 90 -t 3600000 -tl 60000 --thumb none -n -bm -o /home/pi/Downloads/TL" +str(capture_number) + "%04d.jpg")
-    print("Raspistill timelapse done")
-    hide_busy()
+    def lapse(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspistill timelapse starts")
+        os.system("raspistill -rot 90 -t 3600000 -tl 60000 --thumb none -n -bm -o /home/pi/Downloads/TL" +str(capture_number) + "%04d.jpg")
+        print("Raspistill timelapse done")
+        self.hide_busy()
 
-def long_preview():
-    show_busy()
-    print("30 second preview")
-    os.system("raspistill -f -rot 90 -t 30000")
-    hide_busy()
+    def long_preview(self):
+        self.show_busy()
+        print("30 second preview")
+        os.system("raspistill -f -rot 90 -t 30000")
+        self.hide_busy()
 
-def capture_image():
-    
-    show_busy()
-    capture_number = timestamp()
-    print("Raspistill starts")
-    os.system("raspistill -f -rot 90 -o /home/pi/Downloads/" +str(capture_number) + "cam.jpg")
-    print("Raspistill done")
-    hide_busy()
+    def capture_image(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspistill starts")
+        os.system("raspistill -f -rot 90 -o /home/pi/Downloads/" +str(capture_number) + "cam.jpg")
+        print("Raspistill done")
+        self.hide_busy()
 
-def takePicture(channel):
-    print ("Button event callback")
-    capture_number = timestamp()
-    print("Raspistill starts")
-    os.system("raspistill -f -rot 90 -t 3000 -o /home/pi/Downloads/" +str(capture_number) + "cam.jpg")
-    print("Raspistill done")
+    def takePicture(self, channel):
+        print ("Button event callback")
+        capture_number = self.timestamp()
+        print("Raspistill starts")
+        os.system("raspistill -f -rot 90 -t 3000 -o /home/pi/Downloads/" +str(capture_number) + "cam.jpg")
+        print("Raspistill done")
 
-def picture_left():
-    global picture_index 
-    picture_index += 1
-    global shown_picture
-    shown_picture = saved_pictures[picture_index]
-    picture_gallery = Picture(gallery, width=300, height=200, image=shown_picture, grid=[1,0])
+    def picture_left(self):
+        self.picture_index += 1
+        self.shown_picture = self.saved_pictures[self.picture_index]
+        # self.picture_gallery = Picture(self.gallery, width=300, height=200, image=self.shown_picture, grid=[1,0])
 
-def picture_right():
-    global picture_index
-    picture_index += 1
-    global shown_picture
-    shown_picture = saved_pictures[picture_index]
-    picture_gallery = Picture(gallery, width=300, height=200, image=shown_picture, grid=[1,0])
+    def picture_right(self):
+        self.picture_index += 1
+        self.shown_picture = self.saved_pictures[self.picture_index]
+        # self.picture_gallery = Picture(self.gallery, width=300, height=200, image=self.shown_picture, grid=[1,0])
 
-def show_gallery():
-    gallery = Window(app, bg="white", height=300, width=450, layout="grid",title="Gallery")
-    global saved_pictures 
-    saved_pictures = glob.glob('/home/pi/Downloads/*.jpg')
-    global shown_picture 
-    shown_picture = saved_pictures[picture_index] 
-    button_left = PushButton(gallery, grid=[0,0], width=40, height=50, pady=50, padx=10, image="/home/pi/piDSLR/icon/left.png", command=picture_left)    
-    picture_gallery = Picture(gallery, width=300, height=200, image=shown_picture, grid=[1,0]) 
-    button_right = PushButton(gallery, grid=[2,0], width=40, height=50, pady=50, padx=10, image="/home/pi/piDSLR/icon/right.png", command=picture_right) 
-    gallery.show()
+    def show_gallery(self):
+        self.gallery = Window(self.app, bg="black", height=300, width=450, layout="grid",title="Gallery")
+        self.saved_pictures = glob.glob('/home/pi/Downloads/*.jpg')
+        self.shown_picture = self.saved_pictures[self.picture_index] 
+        button_left = PushButton(self.gallery, grid=[0,0], width=40, height=50, pady=50, padx=10, image="/home/pi/piDSLR/icon/left.png", command=self.picture_left)    
+        self.picture_gallery = Picture(self.gallery, width=300, height=200, image=self.shown_picture, grid=[1,0]) 
+        button_right = PushButton(self.gallery, grid=[2,0], width=40, height=50, pady=50, padx=10, image="/home/pi/piDSLR/icon/right.png", command=self.picture_right) 
+        self.gallery.show()
 
-def video_capture():
-    
-    show_busy()
-    capture_number = timestamp()
-    print("Raspivid starts")
-    os.system("raspivid -f -rot 90 -t 30000 -o /home/pi/Downloads/" +str(capture_number) + "vid.h264")
-    print("done")
-    hide_busy()
+    def video_capture(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspivid starts")
+        os.system("raspivid -f -rot 90 -t 30000 -o /home/pi/Downloads/" +str(capture_number) + "vid.h264")
+        print("done")
+        self.hide_busy()
 
-def upload():
-    
-    show_busy()
-    subprocess.Popen(["python3", "/home/pi/piDSLR/dropbox.py", "--yes"])
-    hide_busy()
-    
+    def upload(self):
+        self.show_busy()
+        subprocess.Popen(["python3", "/home/pi/piDSLR/dropbox.py", "--yes"])
+        self.hide_busy()
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-GPIO.add_event_detect(16, GPIO.FALLING, callback=takePicture, bouncetime=2500)
-    
-app = App(layout="grid", title="Camera Controls", bg="black", width=480, height=320)
-
-text0 = Text(app,color="white", grid=[1,0], text="- PiDSLR -")
-
-button1 = PushButton(app, grid=[1,1], width=110, height=110, pady=35, padx=10, image="/home/pi/piDSLR/icon/prev.png", command=long_preview)
-text1 = Text(app, color="white", grid=[1,2],text="Focus")
-#space1 = PushButton(app, grid=[2,0], width=10, height=35, image="/home/pi/piDSLR/icon/100black.png", command=fullscreen)
-
-button2 = PushButton(app, grid=[3,1], width=110, height=110, pady=35, padx=10, image="/home/pi/piDSLR/icon/gallery.png", command=show_gallery)
-text2 = Text(app, color="white", grid=[3,2],text="Gallery")
-#space2 = PushButton(app, grid=[4,0], width=10, height=10, image="/home/pi/piDSLR/icon/100black.png", command=long_preview)
-
-
-button3 = PushButton(app, grid=[5,1], width=110, height=110,  pady=35, padx=10, image="/home/pi/piDSLR/icon/vid.png", command=video_capture)
-text2 = Text(app, color="white", grid=[5,2],text="HD 30s")
-#space3 = PushButton(app, grid=[6,0], width=10, height=10, image="/home/pi/piDSLR/icon/100black.png", command=long_preview)
-
-
-button4 = PushButton(app, grid=[7,1], width=110, height=110, pady=35, padx=10, image="/home/pi/piDSLR/icon/lapse.png", command=burst)
-text3 = Text(app, color="white", grid=[7,2],text="Burst")
-#space4 = PushButton(app, grid=[8,0], width=10, height=10, image="/home/pi/piDSLR/icon/100black.png", command=long_preview)
-
-
-button5 = PushButton(app, grid=[1,3], width=110, height=110, image="/home/pi/piDSLR/icon/self.png", command=lapse)
-text4 = Text(app, color="white", grid=[1,4],text="1h 60pix")
-
-button6 = PushButton(app, grid=[3,3], width=110, height=110, image="/home/pi/piDSLR/icon/long.png", command=split_hd_30m)
-text2 = Text(app, color="white", grid=[3,4],text="HD 30m in 5s")
-
-button7 = PushButton(app, grid=[5,3], width=110, height=110, image="/home/pi/piDSLR/icon/drop.png", command=upload)
-text3 = Text(app, color="white", grid=[5,4],text="Upload")
-
-button8 = PushButton(app, grid=[7,3], width=110, height=110, image="/home/pi/piDSLR/icon/del.png", command=clear)
-text4 = Text(app, color="white", grid=[7,4],text="Clear Folder")
-
-busy = Window(app, bg="red",  height=175, width=480, title="busy")
-
-app.tk.attributes("-fullscreen", True)
-busy.hide()
-app.display()
+if __name__ == '__main__':
+    standalone_app = piDSLR()
+    standalone_app.run()
