db03ca9afc16bfdf7c7e0a127a51ad9de5e4e983
Nick Engmann <cyengmann@gmail.com>
2020-07-10 00:03:43 -0400

    ammended instructions, fritzing, and INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,11 @@ cp -rf icon /home/pi/piDSLR
 cp dropbox_upload.py /home/pi/piDSLR/dropbox_upload.py
 cp pidslr.py /home/pi/piDSLR/pidslr.py
 echo  "Installing Pip Requirements"
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-install pidslr.py /usr/bin/
+python3 -m pip install --upgrade Pillow
 echo  "Installing piDSLR AutoStart service"
+install pidslr.py /usr/bin/
 mkdir -p /home/pi/.config/autostart
 install -m 644 *.desktop /home/pi/.config/autostart/
 sed -i -e '$i \start_x=1\ngpu_mem=128\n' /boot/config.txt
