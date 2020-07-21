f37c7e2698fcb4516006073c5842ac87936612e8
Nick Engmann <cyengmann@gmail.com>
2020-07-20 23:06:37 -0400

    fix INSTALL script

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,10 +8,10 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 -m pip install --upgrade Pillow
 echo  "Installing piDSLM AutoStart service"
-install pidslm.py /usr/bin/
-mkdir -p /home/pi/.config/autostart
-install -m 644 *.desktop /home/pi/.config/autostart/
-sed -i -e '$i \start_x=1\ngpu_mem=128\n' /boot/config.txt
+sudo install pidslm.py /usr/bin/
+sudo mkdir -p /home/pi/.config/autostart
+sudo install -m 644 *.desktop /home/pi/.config/autostart/
+sudo sed -i -e '$i \start_x=1\ngpu_mem=128\n' /boot/config.txt
 echo "Installation Finished, Rebooting Now"
 sleep 2
 reboot now
